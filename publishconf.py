478a115600cd378c08c130775cfc7e49fc8a2365
Chris Patti <cpatti@gmail.com>
2015-06-19 22:56:36 -0400

    Fix output path so the site gets created when I publish.

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://www.feoh.org'
+OUTPUT_PATH = "/opt/blog/blindnotdumb"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
